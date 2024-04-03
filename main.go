274bafd3a2f5f23e1f154ddeccf8c6bb5e181082
Martin Myrseth <mm@myme.no>
2024-04-04 00:05:24 +0200

    Add basic HTTP client + CLI args
    This commit imports and creates a basic HTTP client to fetch (GET) a URL
    passed as a command line argument. It prints the body on success.
    
    Usage:
    
    ```
    ./goat https://myme.no
    ```

--- main.go
+++ main.go
@@ -1,9 +1,47 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"net/http"
+	"os"
 )
 
+func GetUrl(url string) (string, error) {
+	client := http.Client{}
+
+	res, err := client.Get(url)
+	if err != nil {
+		return "", err
+	}
+
+	lines := ""
+	reader := bufio.NewReader(res.Body)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			if err.Error() == "EOF" {
+				break
+			}
+			return "", err
+		}
+		lines += line
+	}
+
+	return lines, nil
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: goat <url>")
+		return
+	}
+
+	res, err := GetUrl(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(res)
 }
