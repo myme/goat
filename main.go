7015cfdcde7ea704fceb7f545b72b9534372be62
Martin Myrseth <mm@myme.no>
2024-04-06 21:42:04 +0200

    Factor out print logic into functions
    Just a small refactor to make things a bit more self-documenting and
    readable.

--- main.go
+++ main.go
@@ -17,6 +17,23 @@ func Distance(l1 goat.Location, l2 goat.Location) float64 {
 	return math.Sqrt(math.Pow(x, 2) + math.Pow(y, 2))
 }
 
+func PrintAddresses(addresses []goat.Address, loc goat.Location) {
+	for i, address := range addresses {
+		distance := Distance(loc, address.Loc)
+		fmt.Printf("%d: %.2f: %s, %s %s\n", i + 1, distance, address.Text, address.PostCode, address.PostText)
+		fmt.Printf("  %f, %f\n", address.Loc.Lat, address.Loc.Lon)
+	}
+}
+
+func PrintPlaces(places []goat.Place) {
+	for _, place := range places {
+		if place.CouldHaveGoats() {
+			fmt.Printf("%s %s\n", place.Type, place.Name)
+			fmt.Printf("%10.1fm pos: %f,%f\n", place.Distance, place.Loc.Lat, place.Loc.Lon)
+		}
+	}
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: goat <address> <...address>")
@@ -48,15 +65,13 @@ func main() {
 
 	// Sort addresses by distance to IP location
 	slices.SortFunc(*addresses.Ok, func(a, b goat.Address) int {
-		return cmp.Compare(Distance((*ip.Ok).Loc, a.Loc), Distance((*ip.Ok).Loc, b.Loc))
+		distA := Distance((*ip.Ok).Loc, a.Loc)
+		distB := Distance((*ip.Ok).Loc, b.Loc)
+		return cmp.Compare(distA, distB)
 	})
 
-	// Print addresses
-	for _, address := range *addresses.Ok {
-		distance := Distance((*ip.Ok).Loc, address.Loc)
-		fmt.Printf("%.2f: %s, %s %s\n", distance, address.Text, address.PostCode, address.PostText)
-		fmt.Printf("  %f, %f\n", address.Loc.Lat, address.Loc.Lon)
-	}
+	// Select an address to search for goats nearby
+	PrintAddresses(*addresses.Ok, (*ip.Ok).Loc)
 
 	// Find places near selected address
 	places := <-goat.Places((*addresses.Ok)[0].Loc)
@@ -67,11 +82,5 @@ func main() {
 
 	// Sort places by distance
 	goat.SortPlaces(*places.Ok)
-
-	for _, place := range *places.Ok {
-		if place.CouldHaveGoats() {
-			fmt.Printf("%s %s\n", place.Type, place.Name)
-			fmt.Printf("%10.1fm pos: %f,%f\n", place.Distance, place.Loc.Lat, place.Loc.Lon)
-		}
-	}
+	PrintPlaces(*places.Ok)
 }
