8b83dd3906a789c22b140f577cce67562b0f771e
Martin Myrseth <mm@myme.no>
2024-04-13 21:43:29 +0200

    Update location tests

--- src/location_test.go
+++ src/location_test.go
@@ -1,46 +1,33 @@
 package goat
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestParseLocation(t *testing.T) {
-	// Valid location, sunshine case
-	{
+	t.Run("ParseLocation() valid location", func (t *testing.T) {
 		loc, _ := ParseLocation("59.9127,10.7461")
 		expected := Location{Lat: 59.9127, Lon: 10.7461}
 		if *loc != expected {
 			t.Fatalf("expected %v, got %v", expected, loc)
 		}
+	})
+
+	// Invalid tests cases
+	invalidLocations := []string{
+		"",
+		"59.9127",
+		"59.9127,10.7461,1337",
+		"abcdef",
 	}
 
-	// Invalid location, empty
-	{
-		_, err := ParseLocation("")
-		if err.Error() != "invalid location format: " {
-			t.Fatalf("unexpected error, got: %s", err.Error())
-		}
-	}
-
-	// Invalid location, single field
-	{
-		_, err := ParseLocation("59.9127")
-		if err.Error() != "invalid location format: 59.9127" {
-			t.Fatalf("unexpected error, got: %s", err.Error())
-		}
-	}
-
-	// Invalid location, too many fields
-	{
-		_, err := ParseLocation("59.9127,10.7461,1337")
-		if err.Error() != "invalid location format: 59.9127,10.7461,1337" {
-			t.Fatalf("unexpected error, got: %s", err.Error())
-		}
-	}
-
-	// Invalid location, alphbetical
-	{
-		_, err := ParseLocation("abcdef")
-		if err.Error() != "invalid location format: abcdef" {
-			t.Fatalf("unexpected error, got: %s", err.Error())
-		}
+	for _, loc := range invalidLocations {
+		t.Run(fmt.Sprintf("ParseLocation() invalid location %s", loc), func (t *testing.T) {
+			_, err := ParseLocation(loc)
+			if err.Error() != fmt.Sprintf("invalid location format: %s", loc) {
+				t.Fatalf("unexpected error, got: %s", err.Error())
+			}
+		})
 	}
 }
